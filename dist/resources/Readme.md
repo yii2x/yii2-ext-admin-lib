2bba6171ef7bacc781beffceb9736a775444f17a
Yuriy Basov <basowy@gmail.com>
2017-05-06 20:12:26 -0700

    remove

--- dist/resources/Readme.md
+++
@@ -1,3 +0,0 @@
-# ext-theme-neptune-05252940-0ab0-479c-9e96-935b0bd3b9d2/resources
-
-This folder contains static resources (typically an `"images"` folder as well).
--- views/Component.php
+++
@@ -1,6 +0,0 @@
-<?php
-use yii\web\View;
-$this->registerJs("Ext.globalEvents.on('app.launch', function(app){ var cmp = app.createCmp(".$config.");});", View::POS_END, $id . '-handler');
-?>
-
-<div id="<?= $id ?>"></div>
