5b0530337ef4ac600911dbd7d9db95b8f8e43c88
Yuriy Basov <basowy@gmail.com>
2017-05-08 19:48:41 -0700

    menu grid tree

--- README.md
+++ README.md
@@ -10,13 +10,13 @@ The preferred way to install this extension is through [composer](http://getcomp
 Either run
 
 ```
-php composer.phar require --prefer-dist yii2x/yii2-ext-admin-lib "*"
+php composer.phar require --prefer-dist yii2x/yii2-ext-admin-lib "@dev"
 ```
 
 or add
 
 ```
-"yii2x/yii2-ext-admin-lib": "*"
+"yii2x/yii2-ext-admin-lib": "@dev"
 ```
 
 to the require section of your `composer.json` file.
---
+++ assets/ExtJsAdminAsset.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Yuriy Basov <basowy@gmail.com>
+ * @since 1.0.0
+ */
+
+
+namespace yii2x\ui\ext\assets;
+
+use yii\web\AssetBundle;
+
+class ExtJsAdminAsset extends AssetBundle
+{
+    
+    public function init()
+    {
+        if(YII_ENV_DEV){
+            $this->css = [
+                '/devext/bootstrap.css'
+            ];
+            $this->js = [
+                '/devext/ext/ext-all-debug.js',
+                '/devext/ext/ext-theme-neptune.js',
+                '/devext/apploader.js',
+                '/devext/app.js',            
+            ]; 
+        }
+        else{
+
+            $this->sourcePath = '@vendor/yii2x/yii2-ext-admin-lib/dist/';        
+            $this->css = [
+                'resources/AdminApp-all.css'
+            ];
+            $this->js = [
+                'app.js'
+            ];
+
+        }
+
+        parent::init();
+    }    
+    
+}
